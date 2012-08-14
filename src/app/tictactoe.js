fe8be4e7ce2b8d88afde7ee65566a2f4b43d9483
kahlow <tkahlow@gmail.com>
2012-08-13 21:11:17 -0400

    Refactored

--- src/app/tictactoe.js
+++ src/app/tictactoe.js
@@ -8,7 +8,7 @@ TicTacToe.square = {
 	none: ''
 };
 
-TicTacToe.game = (function () {
+TicTacToe.Game = function () {
 	var board = [];
 
 	return {
@@ -19,15 +19,94 @@ TicTacToe.game = (function () {
 					board[i][j] = TicTacToe.square.none;
 				}
 			}
-		},
-		placeTile: function(x, y, tile) {
-			board[x][y] = tile;
-		},
-		getBoard: function() {
+
 			return board;
 		},
-		checkWinner: function() {
+		placeTile: function(x, y, tile) {
+			if (board[x][y] == TicTacToe.square.none) {
+				board[x][y] = tile;
+			}
+			else{
+				return "NOPE";
+			}
+		},
+		newGame: function () {
+			TicTacToe.game.createBoard();
+		},
+		getBoard: function () {
+			return board;
+		},
+		checkWinner: function () {
+			var count = 0;
 
+			for (var i = 0; i < 3; i++){
+				for (var j = 0; j < 3; j++){
+					if (board[j][i] == TicTacToe.square.x){
+						count++;
+					}
+					else if (board[j][i] == TicTacToe.square.o){
+						count--;
+					}
+				}
+
+				if (count == 3){
+					return 'X';
+				}
+				else if (count == -3){
+					return 'O';
+				}
+
+				count = 0;
+
+				for (var j = 0; j < 3; j++){
+					if (board[i][j] == TicTacToe.square.x){
+						count++;
+					}
+					else if (board[i][j] == TicTacToe.square.y){
+						count--;
+					}
+				}
+
+				count = 0;
+			}
+
+			count = 0;
+
+			for (var i = 0; i < 3; i++){
+				if (board[i][i] == TicTacToe.square.x){
+					count++;
+				}
+				else if (board[i][i] == TicTacToe.square.o){
+					count--;
+				}
+			}
+
+			if (count == 3){
+				return 'X';
+			}
+			else if (count == -3){
+				return 'O';
+			}
+
+			count = 0;
+
+			for (var i = 3; i > 0; i--){
+				if (board[i][i] == TicTacToe.square.x){
+					count++;
+				}
+				else if (board[i][i] == TicTacToe.square.o){
+					count--;
+				}
+			}
+
+			if (count == 3){
+				return 'X';
+			}
+			else if (count == -3){
+				return 'O';
+			}
 		}
 	};
-})();
+}();
+
+module.exports = TicTacToe;

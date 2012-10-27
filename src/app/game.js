4faf20ab52f7ca7d38ab3f2846890a9a0d99ef0a
kahlow <tkahlow@gmail.com>
2012-10-26 20:08:04 -0400

    Places X tiles

--- src/app/game.js
+++ src/app/game.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	
+	TicTacToe.Game.newGame();
+	
 	var canvas = $("#gameBoard")[0];
     var ctx = canvas.getContext("2d");
 
@@ -15,11 +18,42 @@ $(document).ready(function() {
     }
     ctx.stroke();
 
-    canvas.addEventListener('click', clickReporter, false);
-    
+    canvas.addEventListener('click', placeTile, false);
+
+	function placeTile(e) {
+
+		var x = Math.floor(e.pageX / 100), 
+			y = Math.floor(e.pageY / 100);
+
+		TicTacToe.Game.placeTile(x, y, TicTacToe.square.x);
+
+		draw();
+	};  
+
+
+	function draw() {
+		var board = TicTacToe.Game.getBoard();
+
+		for (var i = 0; i < 3; i++) {
+			for (var j = 0; j < 3; j++){
+				if (board[i][j] != ""){
+					drawX(i, j);
+				}
+			}
+		}
+
+	};
+
+	function drawX(x, y){
+
+		x *= 100;
+		y *= 100;
+
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		ctx.lineTo(x + 100, y + 100);
+		ctx.moveTo(x + 100, y);
+		ctx.lineTo(x, y + 100);
+		ctx.stroke();
+	};
 });
-
-function clickReporter(e){
-	alert("here");
-};
-

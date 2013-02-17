5a3fb619baae649fa26a41c3087b6d510327c6d9
kahlow <tkahlow@gmail.com>
2013-02-17 11:43:34 -0500

    making jshint happy

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require('express'),
 	app = express();
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendfile('public/index.html');
 });
 
-app.get('/src/lib/:file', function(req, res) {
+app.get('/src/lib/:file', function (req, res) {
     res.sendfile('src/lib/' + req.params.file);
 });
 
-app.get('/src/app/:file', function(req, res) {
+app.get('/src/app/:file', function (req, res) {
     res.sendfile('src/app/' + req.params.file);
 });
 
-app.get('/src/css/:file', function(req, res) {
+app.get('/src/css/:file', function (req, res) {
 	res.sendfile('src/css/' + req.params.file);
 });
 
